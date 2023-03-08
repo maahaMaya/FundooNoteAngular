52f500175fc46b522c1593a82b38d109c764ce26
SOURAV <devbhsourav@gmail.com>
2023-03-08 22:39:32 +0530

    [sourav]modified : loginPage

--- FundooNote/src/app/Component/login/login.component.ts
+++ FundooNote/src/app/Component/login/login.component.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -7,30 +7,17 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent {
-  registerForm!: FormGroup;
-  submitted = false;
-
-
-  constructor(private formBuilder: FormBuilder) { }
+  loginForm !: FormGroup;
+  constructor( ) {}
 
   ngOnInit(): void {
-    this.registerForm = this.formBuilder.group({
-
-      email: ['', [Validators.required, Validators.email]],
-      password: ['', [Validators.required, Validators.minLength(6)]],
-      acceptTerms: [false, Validators.requiredTrue]
-    });
+    this.loginForm = new FormGroup({
+      email : new FormControl("", [Validators.email, Validators.required]),
+      password : new FormControl("", Validators.required)
+    })
   }
 
-  // convenience getter for easy access to form fields
-  get f() { return this.registerForm.controls; }
+  loginSubmit(){
 
-  onSubmit() {
-    this.submitted = true;
-
-    // stop here if form is invalid
-    if (this.registerForm.invalid) {
-      return;
-    }
   }
 }
--- FundooNote/src/app/Component/register/register.component.ts
+++ FundooNote/src/app/Component/register/register.component.ts
@@ -6,34 +6,5 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.scss']
 })
-export class RegisterComponent {
-    registerForm!: FormGroup;
-    submitted = false;
-    
-    constructor(private formBuilder: FormBuilder) { }
-  
-    ngOnInit() {
-      this.registerForm = this.formBuilder.group({
-          title: ['', Validators.required],
-          firstName: ['', Validators.required],
-          lastName: ['', Validators.required],
-          email: ['', [Validators.required, Validators.email]],
-          password: ['', [Validators.required, Validators.minLength(6)]],
-          confirmPassword: ['', Validators.required],
-          acceptTerms: [false, Validators.requiredTrue]
-      });
-  }
-  
-  // convenience getter for easy access to form fields
-  get f() { return this.registerForm.controls; }
-  
-  onSubmit() {
-      this.submitted = true;
-  
-      // stop here if form is invalid
-      if (this.registerForm.invalid) {
-          return;
-      }
-  }
-
+export class RegisterComponent { 
 }
